8d4808933522efecd7110c7f76048cdb7309760d
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-12-06 10:36:46 +0000

    added response status codes

--- MagicVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -5,20 +5,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
 
         [HttpGet("{id:int}")]
-        public VillaDTO GetVilla(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (id == 0) return BadRequest();
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null) return NotFound();
+            return Ok(villa);
         }
     }
 }
