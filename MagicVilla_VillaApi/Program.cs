3b5c33c3d77c5b988526f50d2f5001f543150a80
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-12-11 10:52:04 +0000

    added villa repository interface

--- MagicVilla_VillaApi/Program.cs
+++ MagicVilla_VillaApi/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDBContext>(option => { option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultSQLConnection")); });
 builder.Services.AddControllers().AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<ILogging, Logging>();
---
+++ MagicVilla_VillaApi/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_VillaApi.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaApi.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<List<Villa>> GetAllVillas(Expression<Func<Villa>> filter = null);
+        Task<Villa> GetVilla(Expression<Func<Villa>> filter = null, bool tracked = true);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+    }
+}
