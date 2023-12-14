6e6e421798e08618bedad9b6fc89910d9a245dff
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-12-14 12:05:13 +0000

    installed System.IdentityModel.Tokens.Jwt

---
+++ MagicVilla_VillaApi/Repository/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.Dto;
+using MagicVilla_VillaApi.Repository.IRepository;
+
+namespace MagicVilla_VillaApi.Repository
+{
+    public class UserRepository(ApplicationDBContext db) : IUserRepository
+    {
+        private readonly ApplicationDBContext _db = db;
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Registration(RegistrationRequestDTO registrationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
