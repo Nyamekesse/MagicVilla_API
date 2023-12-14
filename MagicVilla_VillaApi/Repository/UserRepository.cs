12a740e1924c0d34ca5410206627a7f6f9415a15
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-12-14 12:06:04 +0000

    added and completed implementation of signup and login of users

--- MagicVilla_VillaApi/Repository/UserRepository.cs
+++ MagicVilla_VillaApi/Repository/UserRepository.cs
@@ -2,25 +2,68 @@
 using MagicVilla_VillaApi.Models;
 using MagicVilla_VillaApi.Models.Dto;
 using MagicVilla_VillaApi.Repository.IRepository;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
 
 namespace MagicVilla_VillaApi.Repository
 {
-    public class UserRepository(ApplicationDBContext db) : IUserRepository
+    public class UserRepository(ApplicationDBContext db, IConfiguration configuration) : IUserRepository
     {
         private readonly ApplicationDBContext _db = db;
+        private readonly string secretKey = configuration.GetValue<string>("ApiSettings:Secret");
         public bool IsUniqueUser(string username)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers.FirstOrDefault(user => user.Username == username);
+            if (user == null) return true;
+            return false;
         }
 
-        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        public async Task<LocalUser> Registration(RegistrationRequestDTO registrationRequestDTO)
         {
-            throw new NotImplementedException();
+            LocalUser user = new()
+            {
+                Username = registrationRequestDTO.Username,
+                Password = registrationRequestDTO.Password,
+                Name = registrationRequestDTO.Name,
+                Role = registrationRequestDTO.Role,
+            };
+
+            await _db.LocalUsers.AddAsync(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+
         }
 
-        public Task<LocalUser> Registration(RegistrationRequestDTO registrationRequestDTO)
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers.FirstOrDefault(user => user.Username == loginRequestDTO.Username && user.Password == loginRequestDTO.Password);
+            if (user == null) return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+            {
+                new(ClaimTypes.Name, user.Id.ToString()),
+                new(ClaimTypes.Role, user.Role)
+            }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user,
+            };
+
+            return loginResponseDTO;
         }
     }
 }
