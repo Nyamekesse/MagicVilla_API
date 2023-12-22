32b03dce280d33e881e62054e40f3657e3923d12
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-12-22 14:32:01 +0000

    added pagination details to response headers

--- MagicVilla_VillaApi/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_VillaApi/Controllers/v1/VillaAPIController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Text.Json;
 
 
 namespace MagicVilla_VillaApi.Controllers.v1
@@ -49,6 +50,8 @@ namespace MagicVilla_VillaApi.Controllers.v1
                 {
                     villalist = villalist.Where(villa => villa.Name.ToLower().Contains(search.ToLower()));
                 }
+                Pagination pagination = new() { PageNumber = pageNumber, PageSize = pageSize };
+                Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(pagination));
                 _response.Result = _mapper.Map<List<VillaDTO>>(villalist);
                 _response.StatusCode = HttpStatusCode.OK;
                 return Ok(_response);
