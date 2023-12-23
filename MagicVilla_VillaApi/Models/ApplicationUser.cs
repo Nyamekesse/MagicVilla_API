78548efae486c8505938dc72d143802f2bda1e60
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-12-23 23:19:53 +0000

    added ApplicationUser model for IdentityUser

---
+++ MagicVilla_VillaApi/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MagicVilla_VillaApi.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public required string Name { get; set; }
+    }
+}
