8152f648dc9a5c02e512d5385592949ac1b35d34
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-12-25 00:12:50 +0000

    configured and added mapping for ApplicationUser and UserDTO

--- MagicVilla_VillaApi/MappingConfig.cs
+++ MagicVilla_VillaApi/MappingConfig.cs
@@ -14,6 +14,7 @@ namespace MagicVilla_VillaApi
             CreateMap<VillaNumber, VillaNumberDTO>().ReverseMap();
             CreateMap<VillaNumber, VillaNumberCreateDTO>().ReverseMap();
             CreateMap<VillaNumber, VillaNumberUpdateDTO>().ReverseMap();
+            CreateMap<ApplicationUser, UserDTO>().ReverseMap();
         }
     }
 }
