1bb7eed27434fba581ce55fa8ab14c903311bd6a
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-02 22:51:59 +0000

    seperated swagger ui configuartion to seperate file, made api version dynamic

--- MagicVilla_VillaApi/Program.cs
+++ MagicVilla_VillaApi/Program.cs
@@ -8,8 +8,9 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Text;
 
 
@@ -43,38 +44,8 @@ builder.Services.AddApiVersioning(options =>
 builder.Services.AddVersionedApiExplorer(options => { options.GroupNameFormat = "'v'VVV"; options.SubstituteApiVersionInUrl = true; });
 var secretKey = Environment.GetEnvironmentVariable("SECRET");
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description =
-            "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
-            "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-            "Example: \"Bearer 12345abcdef\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Scheme = "Bearer"
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
-        {
-            new OpenApiSecurityScheme{
-            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header},
-           new List<string>()
-        }
-    });
-
-    options.SwaggerDoc("v1", new OpenApiInfo { Version = "v1.0", Title = "Magic Villa V1", Description = "API to manage Villa" });
-    options.SwaggerDoc("v2", new OpenApiInfo { Version = "v2.0", Title = "Magic Villa V2", Description = "API to manage Villa" });
-});
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication(x =>
 {
     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
